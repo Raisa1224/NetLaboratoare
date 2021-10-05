91671045aeea793fd9f092ae0cdc6c367bc27a43
Quenamoon <behindsyou@gmail.com>
2021-10-05 13:16:55 +0300

    Added Team Constructor

--- Laborator2/Tema2/SoccerData/Team.cs
+++ Laborator2/Tema2/SoccerData/Team.cs
@@ -20,6 +20,16 @@ namespace SoccerData
             //split the string in substrings
             //convert each substring to the corresponding format for the data member
             //set the data members' values
+            
+            string[] Columns = line.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+            Name = Columns[1];
+            P    = Convert.ToInt32(Columns[2]);
+            W    = Convert.ToInt32(Columns[3]);
+            L    = Convert.ToInt32(Columns[4]);
+            D    = Convert.ToInt32(Columns[5]);
+            F    = Convert.ToInt32(Columns[6]);
+            A    = Convert.ToInt32(Columns[8]);
+            Pts  = Convert.ToInt32(Columns[9]);
         }
 
         public int GetPointDifference()
